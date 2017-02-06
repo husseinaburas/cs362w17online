6b1956b9dee0122e34da2e2366787d59b6287698
ClareSBrown <browncla@oregonstate.edu>
2017-02-05 22:47:40 -0800

    Outlined tests for unittest2 and unittest3

--- projects/browncla/dominion/unittest2.c
+++ projects/browncla/dominion/unittest2.c
@@ -36,12 +36,50 @@
   return 0;
 */
 
-// Test for number of buys for players turn decreases by 1 and does not go negative
-// Test that the supply count was decreased by 1 and did not go negative
-// Test that the other players do not gain a card
-// Test that the other players do not lose coins
-// Test that the players amount of coins did not go below 0
-// Test that the number of cards player has increases by 1 (sum of discard, hand, deck)
 int main(){
+    int i;
+    int seed = 1000;
+    int numPlayers = 2;
+    int thisPlayer = 0;
+    int otherPlayer = 1;
+	struct gameState G, testG;
+	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+			sea_hag, tribute, smithy, council_room};
+	// initialize a game state and player cards
+	initializeGame(numPlayers, k, seed, &G);
+
+	// copy the game state to a test case
+	memcpy(&testG, &G, sizeof(struct gameState));
+
+
+// NOTE CHECK RETURN VALUE BEFORE DOING CALCULATIONS
+
+	// ----------- TEST 1: Test for number of buys for players turn decreases by 1  -----------
+
+	// ----------- TEST 2: Test that the number of buys does not go negative -----------
+
+	// ----------- TEST 3: Test that the number of coins for current player decreased appropriately -----------
+
+	// ----------- TEST 4: Test that the current players amount of coins did not go below 0 -----------
+
+	// ----------- TEST 5: Test that the total number of cards the player has increases by 1 (sum of all decks) -----------
+
+	// ----------- TEST 6: Test that the other player does not gain a card -----------
+
+	// ----------- TEST 7: Test that the other player does not lose a card -----------
+
+	// ----------- TEST 8: Test that the supply count for bought card decreased by 1 -----------
+
+	// ----------- TEST 9: Test that the supply count for bought card did not go below 0 -----------
+
+	// ----------- TEST 10: Test that cannot buy a card if number of buys is 0 -----------
+
+	// ----------- TEST 11: Test that cannot buy a card if no cards of that type available -----------
+
+	// ----------- TEST 12: Test that cannot buy a card if player does not have enough money -----------
+
+
+
+
 	return 0;
 }
