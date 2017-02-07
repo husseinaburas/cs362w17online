e3b984062c70bd363c7fa0cddc4046f8ece5d1d1
Scott Merrill <merrisco@flip3.engr.oregonstate.edu>
2017-02-07 14:15:40 -0800

    changes make for assignment 3

--- projects/merrisco/dominion/unittest4.c
+++ projects/merrisco/dominion/unittest4.c
@@ -1,1 +1,38 @@
-// this is for unittest4.c
+/* 
+Program: unittest4.c
+Author: Scott Merrill
+Date: 02/05/17
+
+Description: This program is designed as a test case for isGameOver(). 
+Preconditions: gamestate is initialized properly.
+Postconditions: Printout  to the screen the results of the tests. 
+*/
+
+
+/***************************************************************************/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+
+int main() {
+4	int i;
+    int seed = 101;
+    int numPlayers = 2;
+    struct gameState G;
+    int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+            sea_hag, tribute, smithy, council_room};
+
+    // initialize a game state and player cards
+    initializeGame(numPlayers, k, seed, &G);
+/*****************************************************************************/
+
+
+    return 0;
+
+}
