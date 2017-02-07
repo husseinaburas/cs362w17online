e3bc6f0fd4d6804b8ba2fa558bbe4cec0176f879
CoreySavage <Savageco@oregonstate.edu>
2017-02-07 13:08:26 -0500

    Create testcard1.c
    Added headers and assert_true function

---
+++ projects/savageco/dominion/testcard1.c
@@ -0,0 +1,35 @@
+/*
+  cardtest1.c
+  Corey Savage
+  Assignment 3
+  
+  Description: Testing for Adventurer Card
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+
+void assert_true(int);
+
+int main() {
+
+
+
+}
+
+// Used to print success or failure messages for test. Comparisons will be made within the 
+// function parameters to test results. 
+void assert_true(int bool) {
+  
+  if (bool != 0) {
+    printf("      -Test Passed");
+  }
+  else {
+    printf("      -Test Failed");
+  }
+
+}
