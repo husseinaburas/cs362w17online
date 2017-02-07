bcc37e64b62a0be836c2be59de31c49ca5658f93
hartmanm <hartmami@oregonstate.edu>
2017-02-07 15:21:48 -0500

    updated unittest

--- projects/hartmami/dominion/unittest3.c
+++ projects/hartmami/dominion/unittest3.c
@@ -1,3 +1,30 @@
+/***********************************************************************************************
+**
+**  Author:       Michael Hartman
+**
+**  Date:           2017-02-06
+**
+**  Filename:       unittest3.c
+**
+**  Description:    unittest for the function discardCard() in dominion tests:
+**
+**  verify preconditions
+**  verify correct card is discarded
+**  verify playedCardCount is incremented
+**  verify player1's hand is as expected
+**  verify player1's deck is as expected
+**  verify discardCount is incremented
+**  verify player1 cannot discard more cards than in hand
+**  verify player2 cannot discard more cards than in hand
+**  verify adding 9 bonus adds 9 coins
+**  verify adding -1 bonus produces error
+**  verify error (adding -1 bonus has no effect)
+**  verify trashFlag ensures playedCardCount is NOT incremented
+**  verify trashFlag ensures card is NOT discarded
+**  verify player1's hand is as expected with trashFlag
+**  verify player1's deck is as expected with trashFlag
+**
+***********************************************************************************************/
 
 #include "dominion.h"
 #include "dominion_helpers.h"
@@ -18,6 +45,7 @@ int main()
 	int playerTwoDeckCount = 0;
 	int playerTwoDiscardCount = 0;
 	struct gameState G;
+	struct gameState H;
 	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy, council_room};
 
 	initializeGame(2, k, seed, &G);  // initialize game state for 2 players
@@ -60,6 +88,7 @@ int main()
 
 	printf("	HAND, DECK, AND DISCARD TESTS\n\n");
 
+		// handPos, currentPlayer, gameState, trashFlag
 		discardCard(0, 0, &G, 0);
 
 		printf("Verify mine is at the top of Player 1's discard pile:  ");
@@ -109,6 +138,57 @@ int main()
 		printf("\nplayer 2 deckCount is:  %d", playerTwoDeckCount);
 		printf("\nplayer 2 discardCount is:  %d", playerTwoDiscardCount);
 		printf("\n\n");
+
+	printf("	TRASHFLAG TESTS\n\n");
+
+	  printf("	Make a new gameState\n");
+		initializeGame(2, k, seed, &H);  // initialize game state for 2 players
+
+			playerOneHandCount = H.handCount[0];
+			playerOneDeckCount = H.deckCount[0];
+			playerOneDiscardCount = H.discardCount[0];
+			H.hand[0][0] = mine;
+			playerTwoHandCount = H.handCount[1];
+			playerTwoDeckCount = H.deckCount[1];
+			playerTwoDiscardCount = H.discardCount[1];
+			printf("\nplayer 1 handCount is:  %d", playerOneHandCount);
+			printf("\nplayer 1 deckCount is:  %d", playerOneDeckCount);
+			printf("\nplayer 1 discardCount is:  %d", playerOneDiscardCount);
+			printf("\nplayer 2 handCount is:  %d", playerTwoHandCount);
+			printf("\nplayer 2 deckCount is:  %d", playerTwoDeckCount);
+			printf("\nplayer 2 discardCount is:  %d", playerTwoDiscardCount);
+			printf("\n\n");
+
+	// handPos, currentPlayer, gameState, trashFlag
+	discardCard(0, 0, &H, 1);
+
+	printf("Verify mine is NOT at the top of Player 1's discard pile:  ");
+	testAssert(H.playedCards[0] != mine);  // run testAssert
+	printf("\n");
+
+	printf("Verify Player 1's playedCardCount is 0:  ");
+	testAssert(H.playedCardCount == 0);  // run testAssert
+	printf("\n");
+
+	printf("Verify Player 1 has 4 cards in their hand:  ");
+	testAssert(H.handCount[0] == 4);  // run testAssert
+	printf("\n");
+
+	printf("Verify Player 1 has 5 cards in their deck:  ");
+	testAssert(H.deckCount[0] == 5);  // run testAssert
+	printf("\n");
+
+	printf("Verify Player 1's discardCount is 1:  ");
+	testAssert(H.discardCount[0] == 1);  // run testAssert
+	printf("\n");
+
+	printf("player 1 handCount is:  %d", H.handCount[0]);
+	printf("\nplayer 1 deckCount is:  %d", H.deckCount[0]);
+	printf("\nplayer 1 discardCount is:  %d", H.discardCount[0]);
+	printf("\nplayer 2 handCount is:  %d", H.handCount[1]);
+	printf("\nplayer 2 deckCount is:  %d", H.deckCount[1]);
+	printf("\nplayer 2 discardCount is:  %d", H.discardCount[1]);
+	printf("\n\n");
 
 	return 0;
 }
