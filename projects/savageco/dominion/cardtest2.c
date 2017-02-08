25dba23f1480d90de599a6def8820546475b2c83
CoreySavage <Savageco@oregonstate.edu>
2017-02-07 22:41:56 -0500

    Update cardtest2.c

--- projects/savageco/dominion/cardtest2.c
+++ projects/savageco/dominion/cardtest2.c
@@ -62,21 +62,22 @@ int main() {
 	// Expected result : 0 + 1 = 1
 	printf("Test 4: Correct number of cards in discard pile\n");
 	expected = 1;
+	printf("%d\n", G.discardCount[0]);
 	assert_true(G.discardCount[0] == expected);
 	
 	// Opponent players deck tests
 	
 	// Test 5 : handNum + newDraw - discard = newHandNum
-	// Expected result : 5 + 0 - 0 = 5
+	// Expected result : 0 + 0 - 0 = 0
 	printf("Test 5: Correct number of cards in opponent hand\n");
-	expected = 5;
+	expected = 0;
 	printf("%d\n", G.handCount[1]);
 	assert_true(G.handCount[1] == expected);
 
 	// Test 6 : deckCount - draw = newDeckCount
-	// Expected result : 5 - 0 = 5
+	// Expected result : 10 - 0 = 10
 	printf("Test 6: Correct number of cards in deck\n");
-	expected = 5;
+	expected = 10;
 	printf("%d\n", G.deckCount[1]);
 	assert_true(G.deckCount[1] == expected);
 	
@@ -84,6 +85,7 @@ int main() {
 	// Expected result : 0 + 0 = 0
 	printf("Test 4: Correct number of cards in discard pile\n");
 	expected = 0;
+	printf("%d\n", G.discardCount[1]);
 	assert_true(G.discardCount[1] == expected);
 }
 
