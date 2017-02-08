3944676566ea4509880d1b3ddb5e6bf1d796ad49
CoreySavage <Savageco@oregonstate.edu>
2017-02-07 23:12:38 -0500

    Update cardtest1.c
    removed vars not used

--- projects/savageco/dominion/cardtest1.c
+++ projects/savageco/dominion/cardtest1.c
@@ -17,7 +17,7 @@ void assert_true(int);
 
 int main() {
 	
-	int expected, n, r, p, deckCount, discardCount, handCount;
+	int expected;
 
 	int k[10] = {adventurer, council_room, feast, gardens, mine,
 	             remodel, smithy, village, baron, great_hall};
