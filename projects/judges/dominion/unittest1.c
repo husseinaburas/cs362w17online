c22115e8eedf176922ee371f44f714c4edff652c
Shae Judge <shaejudge@gmail.com>
2017-02-08 02:26:53 -0500

    unittest1 test

--- projects/judges/dominion/unittest1.c
+++ projects/judges/dominion/unittest1.c
@@ -4,32 +4,26 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#include <assert.h>
 
 
 int main() {
 
-  printf("*************************************\n");
-  printf("unitTest2:\n");
-  printf("TESTING -- testing getCost() -- BEGIN\n");
-
-  int cardNames[] = {curse, estate, duchy, province, copper, silver, gold, adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall, minion, steward, tribute, ambassador, cutpurse, embargo, outpost, salvager, sea_hag, treasure_map};
-  int cardCostCalculated[27];
-  int cardCosts[] = {0, 2, 5, 8, 0, 3, 6, 6, 5, 4, 4, 5, 4, 4, 3, 4, 3, 5, 3, 5, 3, 4, 2, 5, 4, 4, 4};
-
-  printf("TESTING - getCost function - cost of each card. \n");
+	printf("Unit Test 1 - getCost()\n");
+  
+	int card[] = {curse, estate, duchy, province, copper, silver, gold, adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall, minion, steward, tribute, ambassador, cutpurse, embargo, outpost, salvager, sea_hag, treasure_map};
+  
+	int cost[] = {0, 2, 5, 8, 0, 3, 6, 6, 5, 4, 4, 5, 4, 4, 3, 4, 3, 5, 3, 5, 3, 4, 2, 5, 4, 4, 4};
 
     int i;
   for (i = 0; i < 27; i++) {
-    //call getCost()
-    cardCostCalculated[i] = getCost(cardNames[i]);
-    printf("TESTING - cost for card # %d\n", cardNames[i]);
-    assertTrue(cardCostCalculated[i], cardCosts[i]);
+	printf("Testing %d - Cost from getCost = %d, Actual cost = %d \n", card[i], getCost(card[i]), cost[i]);
+	
+	if(getCost(card[i]) != cost[i]){
+		printf("ERROR, card %d is incorrect", card[i]);
+	}
 
   }
 
- printf("TESTING--getCost() -- COMPLETE\n\n");
-
-  return 0;
+  return 0; 
 
 }
