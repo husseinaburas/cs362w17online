16e4e3329f15722a79716f01808998b44f4a4b33
Richard Moot <mootr@oregonstate.edu>
2017-02-07 21:06:24 -0800

    Finshed cardtest1 and updated Makefile

--- projects/mootr/dominion/cardtest1.c
+++ projects/mootr/dominion/cardtest1.c
@@ -23,30 +23,55 @@
   }
 
 int main(int argv, char** argc) {
-  struct gameState* testState = newGame();
-  struct gameState* copyState = newGame();
+  struct gameState testState, copyState;
   int kingdomCards[10] = {adventurer, gardens,  embargo, village, minion,
                           mine,       cutpurse, sea_hag, tribute, smithy};
   int randomSeed = 1000;
   int players = 3;
 
-  initializeGame(players, kingdomCards, randomSeed, testState);
-  int handCountBefore = testState->handCount[0];
+  initializeGame(players, kingdomCards, randomSeed, &testState);
+  memcpy(&copyState, &testState, sizeof(struct gameState));
 
   printf("==============\tSTART TESTING\tplaySmithy()\t==============\n");
 
   // Setting first card of first player's hand to be Smithy
-  testState->hand[0][0] = smithy;
+  testState.hand[0][0] = smithy;
   // Playing smithy
-  playSmithy(testState, 0);
+  playSmithy(&testState, 0);
 
-  UNIT_ASSERT(
-      (testState->hand[0][0] == -1),
-      "TEST CASE:\tCheck card is no longer in hand of player\t\t\tRESULT:");
-  UNIT_ASSERT((testState->handCount[0] == handCountBefore + 2),
+  // Checking discard count to ensure the card was not discarded
+  UNIT_ASSERT((testState.discardCount[0] == 0),
+              "TEST CASE:\tCheck card has not been discarded\t\t\t\tRESULT:");
+
+  // Checking player's hand has increased by 2 (3 from drawing, played 1)
+  UNIT_ASSERT((testState.handCount[0] == copyState.handCount[0] + 2),
               "TEST CASE:\tHand count has increased by 2 (1 played, 3 "
               "gained)\t\tRESULT:");
 
-  printf("==============\tSTART TESTING\tplaySmithy()\t==============\n");
+  // Card has made it into the played cards deck
+  UNIT_ASSERT((testState.playedCards[0] == 13),
+              "TEST CASE:\tPlayed card is in played cards deck\t\t\t\tRESULT:");
+
+  // Player's deck has decreased by 3
+  UNIT_ASSERT((testState.deckCount[0] == copyState.deckCount[0] - 3),
+              "TEST CASE:\tDeck has decreased by 3 cards\t\t\t\t\tRESULT:");
+
+  UNIT_ASSERT((testState.deckCount[1] == copyState.deckCount[1]),
+              "TEST CASE:\tPlayer 2 deck count unchanged\t\t\t\t\tRESULT:");
+
+  UNIT_ASSERT((testState.handCount[1] == copyState.handCount[1]),
+              "TEST CASE:\tPlayer 2 hand count unchanged\t\t\t\t\tRESULT:");
+
+  UNIT_ASSERT((testState.deckCount[2] == copyState.deckCount[2]),
+              "TEST CASE:\tPlayer 3 deck count unchanged\t\t\t\t\tRESULT:");
+
+  UNIT_ASSERT((testState.handCount[2] == copyState.handCount[2]),
+              "TEST CASE:\tPlayer 3 hand count unchanged\t\t\t\t\tRESULT:");
+
+  UNIT_ASSERT(
+      (testState.supplyCount[province] == copyState.supplyCount[province]),
+      "TEST CASE:\tProvince piles have not changed\t\t\t\t\tRESULT:");
+
+  printf("==============\tEnd TESTING\tplaySmithy()\t==============\n");
   return 0;
 };
