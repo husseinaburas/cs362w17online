67efab250a5061285dd91411c7023929fb5fac41
David Moon <dmoon@fetchrobotics.com>
2017-02-13 15:33:02 -0800

    Created random tester for the smithy card, and updated makefile.

--- projects/moondav/dominion/unittest1.h
+++ projects/moondav/dominion/unittest1.h
@@ -2,20 +2,27 @@
 #define PASS 1
 #define FAIL 0
 
+struct TestResult{
+    int numPassed;
+    int numFailed;
+};
+
 // testStatus: Pass = 0; Fail = 1
-void customAssert(int testStatus, char *testName, int observed, int expected){
+void customAssert(int testStatus, char *testName, int observed, int expected, struct TestResult *result){
     if(testStatus){
         printf("PASSED TEST %s: observed = %d, expected = %d\n", testName, observed, expected);
+        result->numPassed++;
     }
     else{
         printf("FAILED TEST %s: observed = %d, expected = %d\n", testName, observed, expected);
+        result->numFailed++;
     }
+    printf("passed: %d, failed: %d\n", result->numPassed, result->numFailed);
 }
 
 // Print the current player's hand
-void printPlayerHand(struct gameState *state){
+void printPlayerHand(struct gameState *state, int currentPlayer){
 	int i = 0;
-	int currentPlayer = whoseTurn(state);
     printf("Player hand: \t");
     for(i = 0; i < state->handCount[currentPlayer]; i++){
         printf("%d ", state->hand[currentPlayer][i]);
@@ -24,9 +31,8 @@ void printPlayerHand(struct gameState *state){
 }
 
 // Pirnt the current player's deck
-void printPlayerDeck(struct gameState *state){
+void printPlayerDeck(struct gameState *state, int currentPlayer){
 	int i = 0;
-	int currentPlayer = whoseTurn(state);
     printf("Player deck: \t");
     for(i = 0; i < state->deckCount[currentPlayer]; i++){
         printf("%d ", state->deck[currentPlayer][i]);
