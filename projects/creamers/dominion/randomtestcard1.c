3aa96bb8bb42b8bffd32fe7fd7ed022338eed357
Stephanie Creamer <creamers@oregonstate.edu>
2017-02-15 11:58:19 -0800

    A4: Finish randomcardtest1.c for smithy

--- projects/creamers/dominion/randomtestcard1.c
+++ projects/creamers/dominion/randomtestcard1.c
@@ -36,21 +36,38 @@ int main(int argc, char* argv[]) {
     }
     int seed = atoi(argv[1]);
     srand(seed);
-    int numPlayers = rand() % 4;
+    int numPlayers;
     int testResults[3] = {-1};
-    int testPlayer = rand() % 4;
+    int testPlayer;
 
     int preHandCount = 0, postHandCount = 0;
     int k[10] = {adventurer, council_room, feast, gardens, mine
                , remodel, smithy, village, baron, great_hall};
     struct gameState G;
-    
+
+    do {
+        numPlayers = rand() % 4;
+    }
+    while (numPlayers < 2);
+    testPlayer = rand() % numPlayers;
     printf ("TESTING smithy card:\n");
 #if (NOISY_TEST == 1)
     printf("\nTEST 1: Play smithy, sufficient number of cards are in deck.\n");
 #endif
     memset(&G, 23, sizeof(struct gameState));   // clear the game state
     initializeGame(numPlayers, k, seed, &G); // initialize a new game
+    //setting hands for all players after player 0 -- initalize game only sets hand for player 0
+    for (int i = 1; i < numPlayers; i++)
+    {
+        G.handCount[i] = G.handCount[0];
+        int j = 0;
+        while(j < G.handCount[i])
+        {
+            drawCard(i, &G);
+            j++;
+        }
+        
+    }
     int randSmithyPos = rand() % G.handCount[testPlayer];
     
     //put smithy in hand
