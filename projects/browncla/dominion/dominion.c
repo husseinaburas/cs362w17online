8afb6d59e6c4753b435c22fc716958652c5c8f1f
ClareSBrown <browncla@oregonstate.edu>
2017-02-15 16:33:18 -0800

    fixed dominion file

--- projects/browncla/dominion/dominion.c
+++ projects/browncla/dominion/dominion.c
@@ -1317,10 +1317,6 @@ int playAdventurer(struct gameState *state, int currentPlayer, int handPos){
       // drawn treasure is the number of cards that have been drawn that are treasure cards
       // keep going until 2 treasure cards are drawn
       while(drawntreasure<1){ //BUG
-        //if the deck is empty we need to shuffle discard and add to deck
-        if (state->deckCount[currentPlayer] <1){
-          shuffle(currentPlayer, state);
-        }
         drawCard(currentPlayer, state);
         int cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
         if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
