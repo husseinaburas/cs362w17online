021c34b52481c5e7bf5046e2ccd3076f181415ae
Corwin Perren <caperren@gmail.com>
2017-02-15 23:20:57 -0800

    Added completed adventurer random tester

---
+++ projects/perrenc/dominion/randomtestcard1.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   randomtestcard1.c
+ * Author: corwinperren
+ *
+ * Created on February 15, 2017, 5:24 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
