6f2f2fa142bed8b949b6428bf5ad8cb6252a0da6
Richard Moot <mootr@oregonstate.edu>
2017-02-18 14:21:59 -0800

    Init commit for assignment 4

---
+++ projects/mootr/dominion/randomtestcard1.c
@@ -0,0 +1,13 @@
+/**
+ * Title:	Random tester for playSmithy() card function
+ * Author: Richard Moot
+ * Description:	Automatically runs through a series of randomized tests
+ * 	to test the playSmithy() card function.
+ */
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
---
+++ projects/mootr/dominion/randomtestcard2.c
@@ -0,0 +1,13 @@
+/**
+ * Title:	Random tester for playFeast() card function
+ * Author: Richard Moot
+ * Description:	Automatically runs through a series of randomized tests
+ * 	to test the playFeast() card function.
+ */
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
