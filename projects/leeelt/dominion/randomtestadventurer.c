799c0e16c8a9a8ccee6635927b147183c277a9f5
Elton Lee <eltonkwoklee@gmail.com>
2017-02-19 16:29:19 -0500

    Fix randomtestadventurer

--- projects/leeelt/dominion/randomtestadventurer.c
+++ projects/leeelt/dominion/randomtestadventurer.c
@@ -1,55 +1,62 @@
 /* Author: Elton Lee
- * cardtest1.c
- * unit test for adventurer
+ * randomtestadventurer.c
+ * random testing for adventurer
  */
 
 /*
  * Include the following lines in your makefile:
  *
- * cardtest1: cardtest1.c dominion.o rngs.o
- *      gcc -o cardtest1 -g  cardtest1.c dominion.o rngs.o $(CFLAGS)
+ * randomtestadventurer: randomtestadventurer.c dominion.o rngs.o
+ * gcc -o randomtestadventurer -g randomtestadventurer.c dominion.o rngs.o $(CFLAGS);
  */
 
-#include "dominion.h"
-#include "dominion_helpers.h"
 #include <string.h>
 #include <stdio.h>
-#include <assert.h>
-#include "rngs.h"
 #include <stdlib.h>
 #include <time.h>
+#include <assert.h>
+
+#include "rngs.h"
 #include "randomtestadventurer.h"
+#include "dominion.h"
+#include "dominion_helpers.h"
 
 int main( int argc, char *argv[])
 {
-	// seed program with user input or call srand with time(NULL)
+	// seed program with user input or call srand with time
 	int test_seed;
+	time_t t;
 	if ( argc == 2) {
 		test_seed = atoi(argv[1]);
-		srand(test_seed);
 	} else {
-		srand(time(NULL));
+		test_seed = (unsigned) time(&t);
 	}
+	srand(test_seed);
 
 	int i;
-    // Using rand to get a number = [min_number, max_number]
-	// rand() % (max_number + 1 - min_number) + min_number
-	int deckTreasure = rand() % (6 + 1 - 4) + 4; // enum 4-6 represents copper, silver, gold
-	int deckTreasureCount = rand() % (130 + 1);  // initialize deck with copper, silver, or gold
-	int deckSize = rand() % ((200-deckTreasureCount) + 1);  // initialize deck with 0 - 200 cards
-	int discardTreasure = rand() % (2 + 1);  // initialize discard pile with copper, silver, or gold
-	int discardTreasureCount = rand() % (130 + 1); // intialize discard with 0 - 130 treasure cards
-	int discardCount = rand() % ((200 - discardTreasureCount) + 1); // initialize discard with 0 to 200 treasure cards
 	int numFailedTests = 0;
 	int numPassedTests = 0;
-	for (i = 0; i < 1000; ++i)
-		runCardTest(deckTreasure, deckTreasureCount, deckSize, discardTreasure, discardTreasureCount, discardCount, &numFailedTests, &numPassedTests);
-
+	for (i = 0; i < 1000; ++i){
+		// Using rand to get a number = [min_number, max_number]
+		// rand() % (max_number + 1 - min_number) + min_number
+		int deckTreasure = rand() % (6 + 1 - 4) + 4; // enum 4-6 represents copper, silver, gold
+		int deckTreasureCount = rand() % (130 + 1);  // initialize deck with copper, silver, or gold
+		int deckSize = rand() % ((200-deckTreasureCount) + 1);  // initialize deck with 0 - 200 cards
+		int discardTreasure = rand() % (2 + 1);  // initialize discard pile with copper, silver, or gold
+		int discardTreasureCount = rand() % (130 + 1); // intialize discard with 0 - 130 treasure cards
+		int discardCount = rand() % ((200 - discardTreasureCount) + 1); // initialize discard with 0 to 200 treasure cards
+		if (runCardTest(deckTreasure, deckTreasureCount, deckSize, discardTreasure, discardTreasureCount, discardCount) == -1){
+			++numFailedTests;
+		} else {
+			++numPassedTests;
+		}
+	}
+	printf("\nSeed used: %d\n", test_seed);
 	printf("Number of unit tests passed: %d\nNumber of unit tests failed: %d\n", numPassedTests, numFailedTests);
     return 0;
 }
 
-int runCardTest(int deckTreasure, int deckTreasureCount, int deckSize, int discardTreasure, int discardTreasureCount, int discardSize, int* numFailedTests, int* numPassedTests)
+int runCardTest(int deckTreasure, int deckTreasureCount, int deckSize, int discardTreasure, int discardTreasureCount, int discardSize)
 {
 	printf("RUNNING playAdventurer WITH %d TREASURES(enum = %d) IN DECK AND %d TREASURES(enum = %d) DISCARD\n", deckTreasureCount, deckTreasure, discardTreasureCount, discardTreasure);
 	int i;
@@ -112,12 +119,7 @@ int runCardTest(int deckTreasure, int deckTreasureCount, int deckSize, int disca
 	// Unit Test 1 -> Check that we have the correct number of treasure cards in hand
 	printf("-----TEST 1: CHECK +2 TREASURE CARD TO HAND AND ADVENTURER NO LONGER IN HAND-----\n");
     printf("Number of cards in hand = %d, expected = %d\n", G.handCount[0], startHandCount + expectedTreasure - 1);
-	if (asserttrue(G.handCount[0], startHandCount + expectedTreasure - 1) == 0){
-		*numFailedTests = *numFailedTests + 1;
-	} else {
-		*numPassedTests = *numPassedTests + 1;
-	}
-	;
+	if (asserttrue(G.handCount[0], startHandCount + expectedTreasure - 1) == -1) return -1;
 	printf("Showing current cards in hand...\n");
 	for (i = 0; i< G.handCount[0]; i++){
 		printf("%d ", G.hand[0][i]);
@@ -126,25 +128,24 @@ int runCardTest(int deckTreasure, int deckTreasureCount, int deckSize, int disca
 
 	printf("-----TEST 2: CHECK TOTAL CARDS FOR PLAYER 1 HAS NOT CHANGED-----\n");
 	printf("Total cards for player 1 = %d, expected = %d\n", G.handCount[0] + G.deckCount[0] + G.discardCount[0], totalCards);
-	if (asserttrue(G.handCount[0] + G.deckCount[0] + G.discardCount[0], totalCards) == 0){
-		*numFailedTests = *numFailedTests + 1;
-	} else {
-		*numPassedTests = *numPassedTests + 1;
+	if (asserttrue(G.handCount[0] + G.deckCount[0] + G.discardCount[0], totalCards) == -1) return -1;
 
-	}
 	printf("\n\n");
 
 	return 0;
 }
 
+// checks val1 == val2
+// true returns 0
+// false returns -1
 int asserttrue(int val1, int val2)
 {
     if (val1 != val2) {
 		printf("TEST FAILED \n");
-		return 0;
+		return -1;
 	}
     else {
 		printf("TEST PASSED \n");
-		return 1;
+		return 0;
 	}
 }
