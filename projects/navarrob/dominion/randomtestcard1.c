65bcbedcf6e2e9a72eb03565f37716b3d61baa5b
Rob Navarro <robnavarro25@gmail.com>
2017-02-19 00:20:28 -0700

    Updating random1

--- projects/navarrob/dominion/randomtestcard1.c
+++ projects/navarrob/dominion/randomtestcard1.c
@@ -28,7 +28,7 @@ int main(int argc, char **argv) {
 	}
 
 	else
-		seed = (int) argv[1];
+		seed = argv[1];
 
 	SelectStream(2);
 	PutSeed(seed);
@@ -38,6 +38,7 @@ int main(int argc, char **argv) {
 		int initialDeckCount = 0;
 		int initialHandCount = 0;
 		int initialActions = 0;
+		int r;
 
 		//Set a random amount of players for game
 		numPlayers = (int) floor(Random() * 3 + 2);
@@ -51,7 +52,20 @@ int main(int argc, char **argv) {
 
 		//randomly set hand and deck count
 		initialHandCount = G.handCount[p] = (int) floor(Random() * MAX_HAND + 1);
-		initialDeckCount = G.deckCount[p] = (int) floor(Random() * (MAX_DECK - 3) + 1);
+		initialDeckCount = G.deckCount[p] = (int) floor(Random() * MAX_DECK);
+
+		r = (int) floor(Random() * 10);
+		if (initialDeckCount == 0) {
+			G.discardCount[p] = 3;
+			G.discard[p][0] = smithy;
+			G.discard[p][1] = adventurer;
+			G.discard[p][2] = gold;
+			G.discard[p][3] = village;
+			if (r == 7) {
+				G.discardCount[p]++;
+				G.discard[p][4] = silver;
+			}
+		}
 
 		printf("Test initialized with %d players, %d hand count, %d deck count.\n", numPlayers, initialHandCount, initialDeckCount);
 		printf("Testing smithy with player %d\n", p + 1);
