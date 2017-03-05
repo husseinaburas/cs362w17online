254b233a2d46a759d33be303a03584e5aeadf641
James Stewart <stewajam@flip2.engr.oregonstate.edu>
2017-03-05 08:25:05 -0800

    Assignment 5

--- projects/stewajam/dominion/mcmurrojDominion/unittest1.c
+++ projects/stewajam/dominion/mcmurrojDominion/unittest1.c
@@ -1,139 +1,70 @@
+/***************************************
+* Name: James Stewart
+* Date: 1/31/2017
+* File: cardtest1.c
+* Description: Tests the isGameOver() Function
+***************************************/
+
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dominion.h"
 #include "dominion_helpers.h"
+#include "rngs.h"
+#include "unittest.h"
 
-//Unit test for update coins found in dominion.c
-// Funciton call: int updateCoins(int player, struct gameState *state, int bonus)
-//Note: updateCions function currently starts line 1252 from dominion.c
+int main() {
+    int testResult = FAIL;
+    int observed, i;
+    int seed = 5;
+    int numPlayers = 2;
+	int numCardTypes = 25;
+	struct gameState G, testG;
+	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+			sea_hag, tribute, smithy, council_room};
 
-
-void testUpdateCoins() {
-    printf("----TEST updateCoins Function-----\n");
-
-    int k[10] = {adventurer, gardens, feast, village, minion, mine, steward,
-        sea_hag, tribute, smithy};
-    int seed = 40;
-//---GS1 TEST---- 3 treasure cards and bonus, +inital coin value
-    // Expect coin output: 8 (Copper+Silver+Gold+Bonus = 1+2+3+2=8)
-    int coinExpectedForGS1 = 8;
-    struct gameState *GS1 = newGame();
-    initializeGame(2,k,seed,GS1);
-    int playerGS1 = 0;  //Maxplayers is 4, test 0-3 array indexing
-    int bonusGS1 = 2;  
-    GS1->coins = 1000; //Used to verify a prexisting coin amount, should be set reset
-    GS1->handCount[playerGS1] = 5;  //Test hand of 5 cards (average).
-    GS1->hand[playerGS1][0] = copper;  //Coin value is 1
-    GS1->hand[playerGS1][1] = silver;  //Coin value is 2
-    GS1->hand[playerGS1][2] = gold;  //Coin value is 3
-    GS1->hand[playerGS1][3] = adventurer; //Coin value is 0
-    GS1->hand[playerGS1][4] = province; //test invalid card
-
-   
-
-//---GS2 TEST---- No cards with bonus
-    // Expect coin output:1 0 (No cards in hand)
-    //GS2 Tests for a player with no cards.  No cards in the hand array are assigned,
-    //as it is assumed there are no cards.  The bonus is set to 10, eventhough the player has no cards, test outcome.
-    int coinExpectedForGS2 = 10;
-    struct gameState *GS2 = newGame();
-    initializeGame(2,k,seed,GS2);
-    int playerGS2 = 1;  //Maxplayers is 4, test 0-3 array indexing
-    int bonusGS2 = 10; 
-    GS2->coins = 5; //Used to verify a prexisting coin amount, should be set back to 0
-    GS2->handCount[playerGS2] = 0;  //Test hand of 0 cards (average).
- 
-
-//---GS3 TEST---- Heavy lifting - checks for 75 treasure cards accurate coin values.
-    // Expect coin output: 151 (25*Copper+25*Silver+25*Gold+Bonus = 25+50+75+1 = 151)
-    int coinExpectedForGS3 = 151;
-    struct gameState *GS3 = newGame();
-    initializeGame(2,k,seed,GS3);
-    int playerGS3 = 2;  //Maxplayers is 4, test 0-3 array indexing
-    int bonusGS3 = 1;  
-    GS3->coins = 0; 
-    GS3->handCount[playerGS3] = 75;  
-    
-    for(int cop=0; cop < 25; cop++)  //Assigns 0-24 indexs to copper cards
-    {
-        GS3->hand[playerGS3][cop] = copper;
+	void newHand(){
+    for(i = 0; i < numCardTypes; i++){
+        G.supplyCount[i] = 10;
     }
-    for(int sil=0; sil < 25; sil++)  //Assigns 25-49 indexs to silver cards
-    {
-        GS3->hand[playerGS3][sil+25] = silver;
-    }
-    for(int gol=0; gol < 25; gol++)  //Assigns 50-74 indexs to gold cards
-    {
-        GS3->hand[playerGS3][gol+50] = gold;
-    }
+	}
+			
+	void process(int expected, char *name){
+		// copy the game state to a test case
+		memcpy(&testG, &G, sizeof(struct gameState));
+		observed = isGameOver(&testG);
+		if(observed == expected){testResult = PASS;}
+		else{testResult = FAIL;}
+		assertTrue(testResult, name, observed, expected);
+	}
+			
+	// initialize a game state and player cards
+	initializeGame(numPlayers, k, seed, &G);
+	
+    printf("\n\n----------------- TESTING isGameOver() FUNCTION -----------------\n\n");
+    newHand();
+    G.supplyCount[province] = 0;
+	process(1, "Test 1: Province pile is empty");
+	
+    newHand();
+    G.supplyCount[adventurer] = 0;
+    G.supplyCount[smithy] = 0;
+    G.supplyCount[embargo] = 0;    
+    process(1, "Test 2: 3 supplies piles are empty");
 
-//---GS4 TEST---- Test coin and bonus w/ estate/adv cards
-    // Expect coin output: 8 (Copper+Silver+Gold+Bonus = 1+2+3+1+2+3=12)
-    int coinExpectedForGS4 = 12;
-    struct gameState *GS4 = newGame();
-    initializeGame(2,k,seed,GS4);
-    int playerGS4 = 3;  //Maxplayers is 4, test 0-3 array indexing
-    int bonusGS4 = 0;  
-    GS4->coins = 0; //Verify an inital 0 amount
-    GS4->handCount[playerGS4] = 10;  
-    GS4->hand[playerGS4][0] = copper;  //Coin value is 1
-    GS4->hand[playerGS4][1] = silver;  //Coin value is 2
-    GS4->hand[playerGS4][2] = gold;  //Coin value is 3
-    GS4->hand[playerGS4][3] = adventurer; //Coin value is 0
-    GS4->hand[playerGS4][4] = smithy; //Coin value is 0
-    GS4->hand[playerGS4][5] = copper;  //Coin value is 1
-    GS4->hand[playerGS4][6] = silver;  //Coin value is 2
-    GS4->hand[playerGS4][7] = gold;  //Coin value is 3
-    GS4->hand[playerGS4][8] = adventurer; //Coin value is 0
-    GS4->hand[playerGS4][9] = village; //Coin value is 0
+    newHand();
+    G.supplyCount[duchy] = 0;
+    G.supplyCount[adventurer] = 0;
+    G.supplyCount[village] = 0;    
+    process(1, "Test 3: 1 empty kingdom pile & 2 empty supplies piles");
 
+    newHand();
+    G.supplyCount[mine] = 0;
+    G.supplyCount[village] = 0;    
+    process(0, "Test 4: 2 supplies piles are empty");    
 
-
-
-    updateCoins(playerGS1, GS1, bonusGS1); // Expect coin output: 8 (Copper+Silver+Gold+Bonus = 1+2+3+2=8)
-    if(GS1->coins == coinExpectedForGS1){
-        printf("PASS for parameters in Game State 1 sent.  Coins Expected:%i  Coins Returned:%i \n", coinExpectedForGS1, GS1->coins);
-    }
-    else
-    {
-        printf("FAIL for parameters in Game State 1 sent.  Coins Expected:%i  Coins Returned:%i \n", coinExpectedForGS1, GS1->coins);
-    }
-
-    updateCoins(playerGS2, GS2, bonusGS2); // Expect coin output: 8 (Copper+Silver+Gold+Bonus = 1+2+3+2=8)
-    if(GS2->coins == coinExpectedForGS2){
-        printf("PASS for parameters in Game State 2 sent.  Coins Expected:%i  Coins Returned:%i \n", coinExpectedForGS2, GS2->coins);
-    }
-    else
-    {
-        printf("FAIL for parameters in Game State 2 sent.  Coins Expected:%i  Coins Returned:%i \n", coinExpectedForGS2, GS2->coins);
-    }
-
-    updateCoins(playerGS3, GS3, bonusGS3); // Expect coin output: 8 (Copper+Silver+Gold+Bonus = 1+2+3+2=8)
-    if(GS3->coins == coinExpectedForGS3){
-        printf("PASS for parameters in Game State 3 sent.  Coins Expected:%i  Coins Returned:%i \n", coinExpectedForGS3, GS3->coins);
-    }
-    else
-    {
-        printf("FAIL for parameters in Game State 3 sent.  Coins Expected:%i  Coins Returned:%i \n", coinExpectedForGS3, GS3->coins);
-    }
-
-    updateCoins(playerGS4, GS4, bonusGS4); // Expect coin output: 8 (Copper+Silver+Gold+Bonus = 1+2+3+2=8)
-    if(GS4->coins == coinExpectedForGS4){
-        printf("PASS for parameters in Game State 4 sent.  Coins Expected:%i  Coins Returned:%i \n", coinExpectedForGS4, GS4->coins);
-    }
-    else
-    {
-        printf("FAIL for parameters in Game State 4 sent.  Coins Expected:%i  Coins Returned:%i \n", coinExpectedForGS4, GS4->coins);
-    }
- 
-    return;
-}
-
-int main(int argc, char *argv[])
-{
-    testUpdateCoins();
+	printf(" >>>>> SUCCESS: Testing isGameOver() complete <<<<<\n\n");
+	
     return 0;
 }
-
-
-
--- projects/stewajam/dominion/mcmurrojDominion/unittest2.c
+++ projects/stewajam/dominion/mcmurrojDominion/unittest2.c
@@ -1,64 +1,111 @@
+/***************************************
+* Name: James Stewart
+* Date: 1/31/2017
+* File: cardtest1.c
+* Description: Tests the updateCoins() Function
+***************************************/
+
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "dominion.h"
 #include "dominion_helpers.h"
+#include "rngs.h"
+#include "unittest.h"
 
-
-//Unit test for getCost found in dominion.c
-// Funciton call: int getCost(int cardNumber)
-//Note: testgetCost function currently starts line 5893 from dominion.c
-
-void testgetCost() {
-	printf("----TEST getCost Function-----\n");
+int main() {
+    int testResult = FAIL;
+    int observed, i, bonus, card1, card2, card3, card4, card5;
+    int seed = 5;
+    int numPlayers = 2;
+	struct gameState G, testG;
+	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+			sea_hag, tribute, smithy, council_room};
+    int currentPlayer = whoseTurn(&G);
 	
-    int costArray[27] = {0,2,5,8,0,3,6,6,5,4,4,5,4,4,3,4,3,5,3,5,3,4,2,5,4,4,4};  // Costs referenced from http://dominion.diehrstraits.com/?set=All&f=list
-    char*NameArray[27] = {"curse","estate","duchy","province","copper",
-    "silver","gold","adventurer","council_room","feast","gardens","mine",
-    "remodel","smithy","village","baron","great_hall","minion","steward",
-    "tribute","ambassador","cutpurse","embargo","outpost","salvager","sea_hag","treasure_map"};
-    char *(*pointerToNameArray)[27] = &NameArray;
+	void newHand(){
+		// copy the game state to a test case
+		G.hand[currentPlayer][0] = card1;
+		G.hand[currentPlayer][1] = card2;
+		G.hand[currentPlayer][2] = card3;
+		G.hand[currentPlayer][3] = card4;
+		G.hand[currentPlayer][4] = card5;
+		G.handCount[currentPlayer] = 5;
+		memcpy(&testG, &G, sizeof(struct gameState));
+		printf("\nPlayer hand: \t");
+		for(i = 0; i < G.handCount[currentPlayer]; i++){
+			printf("%d ", G.hand[currentPlayer][i]);
+		}
+		printf("\nExpected hand: \t%d %d %d %d %d\n\n", card1, card2, card3, card4, card5);
+		updateCoins(currentPlayer, &testG, bonus);
+	}
+	
+	void process(int expected){
+		observed = testG.coins;
+		if(observed == expected){testResult = PASS;}
+		else{testResult = FAIL;}
+		assertTrue(testResult, "Coins", observed, expected);
 
-   for(int i=0; i <27; i++)
-   {
-   		int tempCost = getCost(i);
+		observed = testG.playedCardCount;
+		expected = G.playedCardCount;
+		if(observed == expected){testResult = PASS;}
+		else{testResult = FAIL;}
+		assertTrue(testResult, "Played Cards", observed, expected);
 
-   		if(tempCost == costArray[i])
-   		{
-   		printf("PASS for parameters %s.  Cost Expected:%i  Cost Returned:%i \n",(*pointerToNameArray)[i], costArray[i], tempCost);
-   		}
+		observed = testG.discardCount[currentPlayer];
+		expected = G.discardCount[currentPlayer];
+		if(observed == expected){testResult = PASS;}
+		else{testResult = FAIL;}
+		assertTrue(testResult, "Discards", observed, expected);
 
-    	else
-    	{
-    	printf("FAIL for parameters %s.  Cost Expected:%i  Cost Returned:%i \n",(*pointerToNameArray)[i], costArray[i], tempCost);
-    	}
-   }
-   
-   int outsideOfCardNumBounds = 30;
-   int expectedReturnOfOutsideBounds = -1;
-   int costOutsideBounds = getCost(outsideOfCardNumBounds); 
+		observed = testG.deckCount[currentPlayer];
+		expected = G.deckCount[currentPlayer];
+		if(observed == expected){testResult = PASS;}
+		else{testResult = FAIL;}
+		assertTrue(testResult, "Deck Count", observed, expected);
 
-   if(costOutsideBounds == expectedReturnOfOutsideBounds)
-   	{
-   	printf("PASS for invalid vard.  Return Value Expected:%i   Return Value:%i \n",expectedReturnOfOutsideBounds, costOutsideBounds);
-   	}
+		observed = testG.handCount[currentPlayer];
+		expected = G.handCount[currentPlayer];
+		if(observed == expected){testResult = PASS;}
+		else{testResult = FAIL;}
+		assertTrue(testResult, "Hand Count", observed, expected);
+	}
+	
+	// initialize a game state and player cards
+	initializeGame(numPlayers, k, seed, &G);
+    printf("\n\n----------------- TESTING updateCoins() FUNCTION -----------------\n\n");
+    printf("TEST 1: 1 copper, 1 silver, 1 gold, 0 bonus\n");
+    card1 = copper;
+    card2 = silver;
+    card3 = gold;
+    card4 = adventurer;
+    card5 = council_room;
+	bonus = 0;
+	newHand();
+	process(6);
 
-    else
-    {
-    printf("FAIL for invalid vard.  Return Value Expected:%i   Return Value:%i \n",expectedReturnOfOutsideBounds, costOutsideBounds);
-    }
+    printf("\nTEST 2: 3 copper, bonus 2\n");
+    card1 = copper;
+    card2 = smithy;
+    card3 = copper;
+    card4 = estate;
+    card5 = copper;
+	bonus = 2;
+	newHand();
+	process(5);
 
-    return;
-}
+    printf("\nTEST 3: 0 treasure, bonus 5\n");
+    card1 = province;
+    card2 = smithy;
+    card3 = feast;
+    card4 = estate;
+    card5 = tribute;
+	bonus = 5;
+	newHand();
+	process(5);
 
+	printf(" >>>>> SUCCESS: Testing updateCoins() complete <<<<<\n\n");
 
-
-
-
-
-
-int main(int argc, char *argv[])
-{
-    testgetCost();
     return 0;
 }
